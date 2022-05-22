fd4867f2af3e1967b43fa5971592ad47b3e5b02e
Nabiulla <snm.supreme@yandex.ru>
2022-05-22 07:02:25 +0300

    Add TODO

--- Source/main.py
+++ Source/main.py
@@ -20,17 +20,20 @@ def start_driver():
         driver.get(url)
         #time.sleep(2)
         seasons = driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div/div/div[5]/div[2]/div[3]/div[1]/ul").find_elements(By.TAG_NAME, "li")
+        # TODO: Асинхронная обработка сезонов в отдельных вкладках, чтобы не ждать загрузку каждого файла
         for season in seasons:
             season.click()
+            #TODO: Элемент с классом vjs-poster перекрывает элемент с тэгом li
             episodes = driver.find_element(By.CLASS_NAME, "tab-content").find_element(By.CLASS_NAME, "active").find_element(By.TAG_NAME, "ul").find_elements(By.TAG_NAME, "li")
             for episode in episodes:
+                #TODO: Обработка ошибки загрузки серии, чтобы загрузка продолжалась
                 link = episode.find_element(By.CLASS_NAME, "margin_b5").find_element(By.TAG_NAME, "a")
                 link.click()
                 time.sleep(2)
                 video_link = driver.find_element(By.ID, "lingonline-video_html5_api").get_attribute("src")
                 video_close_button = driver.find_element(By.CLASS_NAME,"video-close-button")
                 video_close_button.click()
-                #time.sleep(2)
+                time.sleep(1)
                 links.append(video_link)
     except Exception as ex:
         print(ex)
