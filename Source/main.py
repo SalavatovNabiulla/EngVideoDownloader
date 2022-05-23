006e2bcc980071f90dfa1db8372007d1aebf9806
Nabiulla <snm.supreme@yandex.ru>
2022-05-23 23:30:32 +0300

    Create parsing algorithm

--- Source/main.py
+++ Source/main.py
@@ -1,107 +1,92 @@
 #Imports
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
-from threading import Thread
-import time
+import json
+import requests
 import subprocess
 import os
+from bs4 import BeautifulSoup
+import regex
 
 #Public variables
-links = []
-threads = []
+video_links = []
+media_links = []
+data_links = []
 
 #Parsing functions
-def process_current_season(index):
-    # TODO: Добавить прогресс обработки сезона
-    driver = create_driver()
-    driver = set_current_season(driver, index)
-    print("Обработка сезона №"+str(index+1))
-    try:
-        #TODO: Элемент с классом vjs-poster перекрывает элемент с тэгом li
-        episodes = driver.find_element(By.CLASS_NAME, "tab-content").find_element(By.CLASS_NAME, "active").find_element(By.TAG_NAME, "ul").find_elements(By.TAG_NAME, "li")
-        for episode in episodes:
+def start_parsing_video_links():
+    for m_link in media_links:
+        page_path = os.path.dirname(os.path.abspath(__file__)) + '\\page.html'
+        try:
+            os.remove(page_path)
+        except Exception as ex:
+            pass
+        res = requests.get(m_link)
+        with open(page_path,'w', encoding="utf-8") as file:
+            file.write(res.text)
+        with open(page_path,'r', encoding="utf-8") as file:
+            content = file.read()
+            soup = BeautifulSoup(content,'lxml')
+            script = soup.find("script")
+            pattern = regex.compile(r'\{(?:[^{}]|(?R))*\}')
+            test = pattern.findall(script.text)
+            link = str(test[4])
             try:
-                #TODO: Исправить ошибки с перекрыванием элементов
-                #print("Обработка серии №" + str(episodes.index(episode)+1) + " сезона №" + str(index+1))
-                link = episode.find_element(By.CLASS_NAME, "margin_b5").find_element(By.TAG_NAME, "a")
-                link.click()
-                time.sleep(2)
-                video_link = driver.find_element(By.ID, "lingonline-video_html5_api").get_attribute("src")
-                video_close_button = driver.find_element(By.CLASS_NAME,"video-close-button")
-                video_close_button.click()
-                time.sleep(1)
-                links.append(video_link)
+                os.remove("link.txt")
             except Exception as ex:
-                #print("Ошибка обработки серии №"+str(episodes.index(episode)+1)+" сезона №"+str(index+1))
-                continue
-    except Exception as ex:
-        print("Ошибка обработки сезона №"+str(index+1))
-    finally:
-        driver.close()
-def process_all_seasons():
-    for index in range(get_seasons_count()):
-        threads.append(Thread(target=process_current_season, args=(index,)))
-def get_seasons_count():
-    driver = create_driver()
-    driver.get(url)
-    seasons_count = driver.find_element(By.XPATH,"/html/body/div[3]/div[2]/div/div/div[5]/div[2]/div[3]/div[1]/ul").find_elements(By.TAG_NAME, "li")
-    driver.close()
-    return len(seasons_count)
-def set_current_season(driver,index):
-    driver.get(url)
-    seasons = driver.find_element(By.XPATH,"/html/body/div[3]/div[2]/div/div/div[5]/div[2]/div[3]/div[1]/ul").find_elements(By.TAG_NAME, "li")
-    seasons[index].click()
-    return driver
+                pass
+            with open("link.txt","w") as file:
+                file.write(link)
+            with open("link.txt","r") as file:
+                lines = file.readlines()
+                link = lines[1].split(":")
+                link = link[1]+":"+link[2]
+                link = link.replace(',','').replace('"','').replace(' ','')
+                video_links.append(link)
+def create_media_links():
+    host = "https://engvideo.pro"
+    for link in data_links:
+        media_links.append(host+link)
 def start_parsing():
-    try:
-        process_all_seasons()
-    except Exception as ex:
-        print(ex)
+    with open("main.html","r",encoding="utf-8") as file:
+        content = file.read()
+        soup = BeautifulSoup(content,"lxml")
+        seasons = soup.find("div",class_="tab-content").find_all("div",class_="tab-pane")
+        for season in seasons:
+            episodes = season.find("ul").find_all("li")
+            for episode in episodes:
+                episode_href = episode.find("div",class_="clearfix").find("div",class_="pull-left").find("h5",class_="margin_b5").find("a")["data-href"]
+                data_links.append(episode_href)
 
 #Downloading functions
 #TODO: Сортировка серий по папкам
-def start_video_downloading():
-    links_path = os.path.dirname(os.path.abspath(__file__))+'\\links.txt'
+def start_site_downloading():
+    site_path = os.path.dirname(os.path.abspath(__file__)) + '\\main.html'
     try:
-        os.remove(links_path)
+        os.remove(site_path)
+    except Exception as ex:
+        pass
+    res = requests.get(url)
+    with open(site_path,'w', encoding="utf-8") as file:
+         file.write(res.text)
+def start_video_downloading():
+    path = os.path.dirname(os.path.abspath(__file__))+'\\links.txt'
+    try:
+        os.remove(path)
     except Exception as ex:
         print(ex)
-    with open('links.txt','w') as file:
-        for link in links:
+    with open(path,'w') as file:
+        for link in video_links:
             file.write(link+"\n")
-    subprocess.Popen([path_to_exe, links_path])
+    subprocess.Popen([path_to_exe, path])
 
 #Main functions
 #TODO: Автоматическая установка приложения DownloadMaster из установщика или интернета
 #TODO: Автоматическая установка библиотеки Selenium
-def create_driver():
-    os.environ['WDM_LOG_LEVEL'] = '0'
-    service = Service(GeckoDriverManager().install())
-    options = webdriver.FirefoxOptions()
-    options.headless = True
-    options.add_argument("--mute-audio")
-    options.add_argument("--disable-blink-features")
-    driver = webdriver.Firefox(service=service,options=options)
-    return driver
-def start_processing():
-    while len(threads) != 0:
-        for index in range(4):
-            try:
-                threads[index].start()
-            except Exception as ex:
-                continue
-        for index in range(4):
-            try:
-                threads[index].join()
-                threads.pop(index)
-            except Exception as ex:
-                continue
 
 #Main
 url = input("Введите ссылку на сериал: ")
 path_to_exe = "C:\\Program Files (x86)\\Download Master\\dmaster.exe"
+start_site_downloading()
 start_parsing()
-start_processing()
+create_media_links()
+start_parsing_video_links()
 start_video_downloading()
