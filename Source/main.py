e2a570b0926cfefd19196ef025e6e16ecb1d47ef
Nabiulla <snm.supreme@yandex.ru>
2022-05-25 01:22:56 +0300

    Cleaning

--- Source/main.py
+++ Source/main.py
@@ -8,43 +8,43 @@ class series:
 
     # TODO: Добавить функции для загрузки всех сезонов
 
-    def set_seasons(self):
+    def __set_seasons(self):
         soup = BeautifulSoup(self.source,"lxml")
         seasons = soup.find("div",class_="tab-content").find_all("div",class_="tab-pane")
         for index in range(len(seasons)):
             self.seasons.append(season(index+1,seasons[index]))
 
-    def set_source(self,url):
+    def __set_source(self,url):
         result = requests.get(url)
         status_code = result.status_code
         # TODO: Сделать проверку доступности сайта, получая код ответа
         self.source = result.text
-        #
 
     def __init__(self,url):
         self.seasons = []
         self.source = None
         # TODO: Добавить информация о сеансе и Cookie
         #
-        self.set_source(url)
-        self.set_seasons()
+        self.__set_source(url)
+        self.__set_seasons()
 #--
 class season:
 
     #TODO: Добавить функции для загрузки всех серий
 
-    def set_episodes(self):
+    def __set_episodes(self):
         episodes = self.source.find("ul").find_all("li")
         for index in range(len(episodes)):
             self.episodes.append(episode(index+1,self,episodes[index]))
 
     def __init__(self,number,source):
         print("Сбор информации о сезоне номер "+str(number))
+        #--Variables
         self.number = number
         self.source = source
         self.episodes = []
-        #
-        self.set_episodes()
+        #--Functions
+        self.__set_episodes()
 #--
 class episode:
 
@@ -77,7 +77,7 @@ class episode:
         else:
             print("Ошибка загрузки серии (Код ошибки: "+str(status_code)+")")
 
-    def set_video_link(self):
+    def __set_video_link(self):
         result = requests.get(self.data_link)
         status_code = result.status_code
         # TODO: Сделать проверку доступности сайта, получая код ответа
@@ -87,7 +87,7 @@ class episode:
         spl = video_link = str(pattern.findall(script.text)[4]).replace("{","").replace("}","").replace(" ","").replace('"',"").replace(",","").split(":")
         self.video_link = spl[1]+":"+spl[2]
 
-    def set_data_link(self):
+    def __set_data_link(self):
         self.data_link = self.data_link + self.source.find("div", class_="clearfix").find("div", class_="pull-left").find("h5", class_="margin_b5").find("a")["data-href"]
 
     def __init__(self,number,season,source):
@@ -97,8 +97,8 @@ class episode:
         self.data_link = 'https://engvideo.pro'
         self.video_link = ''
         #
-        self.set_data_link()
-        self.set_video_link()
+        self.__set_data_link()
+        self.__set_video_link()
         # TODO: Добавить определение названия серии
 
 #--UserInterface
